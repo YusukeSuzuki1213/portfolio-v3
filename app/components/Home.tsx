4eb2a0c45f626330623c400e201e7435ca397abb
YusukeSuzuki1213 <kikikaikai11110@gmail.com>
2023-04-26 18:03:37 +0900

    fixed home element size

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -4,13 +4,15 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="h-full flex flex-col lg:flex-row justify-center items-center lg:space-x-16">
-      <div className="w-60 h-60 lg:w-64 lg:h-64 rounded-full overflow-hidden bg-white">
+    <div className="h-full flex flex-col lg:flex-row justify-center items-center lg:space-x-20">
+      <div className="w-56 h-56 lg:w-64 lg:h-64 rounded-full overflow-hidden bg-white">
         <Image src="" alt="アバターアイコン" width={500} height={500} />
       </div>
       <div className="flex flex-col items-center space-y-6 mt-6 lg:items-start lg:space-y-10 lg:space-x-0 lg:mt-0">
-        <p className="text-4xl lg:text-5xl">Yusuke Suzuki</p>
-        <p className="italic">Software engineer based in Japan.</p>
+        <p className="text-4xl lg:text-6xl">Yusuke Suzuki</p>
+        <p className="italic text-gray-300">
+          Software engineer based in Japan.
+        </p>
         <div className="flex items-center space-x-4">
           <a
             href="https://twitter.com/"
