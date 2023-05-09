74397653a925db51e0e9785be220c58e34b27c8e
YusukeSuzuki1213 <kikikaikai11110@gmail.com>
2023-05-09 14:43:59 +0900

    add avator circle animation

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -5,7 +5,7 @@ import { serviceAccounts, toAwesomeIcon } from "../constants/serviceAccount";
 export default function Home() {
   return (
     <div className="flex flex-col lg:flex-row justify-center items-center lg:space-x-20">
-      <div className="w-56 h-56 lg:w-64 lg:h-64 rounded-full overflow-hidden bg-white">
+      <div className="w-56 h-56 lg:w-64 lg:h-64 animate-distorted-circle overflow-hidden bg-white">
         <Image src="" alt="アバターアイコン" width={500} height={500} />
       </div>
       <div className="flex flex-col items-center space-y-6 mt-6 lg:items-start lg:space-y-10 lg:space-x-0 lg:mt-0">
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,8 +15,17 @@ module.exports = {
       animation: {
         "slide-left": "slide-left 0.5s cubic-bezier(0.680, -0.550, 0.265, 1.550) both",
         "slide-bottom": "slide-bottom 0.5s cubic-bezier(0.680, -0.550, 0.265, 1.550) both",
+        "distorted-circle": 'distorted-circle 10s ease-in-out 1s infinite'
       },
       keyframes: {
+        "distorted-circle": {
+          "0%, 100%": {
+            "border-radius": "30% 70% 70% 30% / 59% 45% 55% 41%"
+          },
+          "50%": {
+            "border-radius": "57% 43% 78% 22% / 71% 75% 25% 29%"
+          }
+        },
         "slide-left": {
           "0%": {
             transform: "translateX(0px)"
