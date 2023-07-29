5a364da996f8ed7a0be92081c3e439e209f2f666
YusukeSuzuki1213 <kikikaikai11110@gmail.com>
2023-07-29 15:03:39 +0900

    fix photo order

--- app/constants/photos.ts
+++ app/constants/photos.ts
@@ -8,7 +8,7 @@ export const photoList = [
       path: "/travel/philippines/GOPR0246.JPG",
     },
     {
-      path: "/travel/philippines/IMG_0848.jpeg",
+      path: "/travel/vietnam/Z7DmN91NQt6aShnrYMPYWA.jpeg",
     },
     {
       path: "/travel/singapore/4vV3Yb-5RFeqOI9C5mjlmw.jpeg",
@@ -27,7 +27,7 @@ export const photoList = [
   ],
   [
     {
-      path: "/travel/vietnam/Z7DmN91NQt6aShnrYMPYWA.jpeg",
+      path: "/travel/philippines/IMG_0848.jpeg",
     },
     {
       path: "/travel/japan/ZrS0AY_yQSm7luafDXLEWg.jpeg",
