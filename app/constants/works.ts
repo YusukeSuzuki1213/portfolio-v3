4099eed7ed8e96687c9f8941b366626615674252
Yusuke <yusuke.suzuki.2024@gmail.com>
2023-12-12 16:45:37 +0900

    added: motogatari effect

--- app/constants/works.ts
+++ app/constants/works.ts
@@ -15,4 +15,12 @@ export const works: WorksType[] = [
     url: "https://play.google.com/store/apps/details?id=site.mma_news",
     imageUrl: "/japan_mma_news.png",
   },
+  {
+    title: "Monogatari Effect",
+    subtitle:
+      "『物語シリーズ』で登場するアニメーションをWeb Animations APIを用いてTypeScriptで実装しました。",
+    date: "18 Jul 2020",
+    url: "https://www.youtube.com/watch?v=ZcojaVmAoNA",
+    imageUrl: "/monogatari_effect.png",
+  },
 ];
