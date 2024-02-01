32e009ac97275815d9bbb4c12401b3118604c219
Yusuke <yusuke.suzuki.2024@gmail.com>
2024-02-01 23:22:17 +0900

    added: photos

--- app/components/Photos.tsx
+++ app/components/Photos.tsx
@@ -1,8 +1,8 @@
 import Heading from "@/app/components/Heading";
-import Image from "next/image";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
-import { photos } from "@/app/constants/photos";
+import { homeGalleries } from "@/app/constants/gallery";
+import Gallery from "@/app/components/Gallery";
 import Link from "next/link";
 
 export default async function Photos() {
@@ -12,20 +12,7 @@ export default async function Photos() {
         <Heading id="photos" text="Photos" />
       </div>
       <div className="flex flex-wrap flex-col items-center justify-center mx-6">
-        <div className="columns-1 md:columns-3">
-          {photos.map((photo) => {
-            return (
-              <Image
-                key={photo.path}
-                className="h-auto max-w-full rounded-md mb-4"
-                width={512}
-                height={0}
-                src={photo.path}
-                alt=""
-              />
-            );
-          })}
-        </div>
+        <Gallery galleries={homeGalleries} />
         <div className="text-center mt-12 w-full sm:w-1/2 md:w-1/3">
           <Link href="/gallery">
             <button className="text-green-500 hover:text-white border border-green-500 hover:bg-green-500 hover:border-green-500 font-medium rounded-lg py-2.5 text-center text-sm w-full">
--- app/constants/gallery.ts
+++ app/constants/gallery.ts
@@ -19,65 +19,186 @@ export type Gallery = {
   readonly photos: Photo[];
 };
 
-const getPhotos = (folder: string): Photo[] => {
+const getPhotosInFolder = (folder: string): Photo[] => {
   return glob
     .sync(`public/${folder}/*.{jpg,jpeg,png}`)
     .map((path) => {
       return { path: path.replace("public", "") };
     })
-    .slice()
-    .sort((_, __) => 0.5 - Math.random()); // ランダムシャッフル
+    .slice();
 };
+
+export const homeGalleries: Gallery[] = [
+  {
+    category: "latest",
+    categoryDisplayName: "⚡ Latest photos",
+    photos: [
+      { path: "/travel/latest/IMG_1139.JPG" },
+      { path: "/travel/latest/6CzNLn88TYm3x3FODxxzAQ.jpeg" },
+      { path: "/travel/latest/BRydPs8cRM61xhum938FyA.jpeg" },
+      { path: "/travel/latest/H0myozRtSW2UE5qSs8QCFg.jpeg" },
+      { path: "/travel/latest/xyEnCrWKS0GKEt2HhOGo3g.jpeg" },
+    ],
+  },
+  {
+    category: "cambodia",
+    categoryDisplayName: "🇰🇭 Cambodia",
+    photos: [
+      { path: "/travel/cambodia/2.jpeg" },
+      { path: "/travel/cambodia/3.jpeg" },
+      { path: "/travel/cambodia/4.jpeg" },
+      { path: "/travel/cambodia/5.jpeg" },
+      { path: "/travel/cambodia/12.jpeg" },
+      { path: "/travel/cambodia/13.jpeg" },
+      { path: "/travel/cambodia/15.jpeg" },
+    ],
+  },
+  {
+    category: "japan",
+    categoryDisplayName: "🇯🇵 Japan",
+    photos: [
+      { path: "/travel/japan/11.jpeg" },
+      { path: "/travel/japan/23.jpeg" },
+      { path: "/travel/japan/42.jpeg" },
+      { path: "/travel/japan/64.jpeg" },
+      { path: "/travel/japan/68.jpeg" },
+      { path: "/travel/japan/72.jpeg" },
+      { path: "/travel/japan/103.jpeg" },
+      { path: "/travel/japan/106.jpeg" },
+    ],
+  },
+  {
+    category: "malaysia",
+    categoryDisplayName: "🇲🇾 Malaysia",
+    photos: [
+      { path: "/travel/malaysia/2.jpeg" },
+      { path: "/travel/malaysia/3.jpeg" },
+      { path: "/travel/malaysia/5.jpeg" },
+      { path: "/travel/malaysia/13.jpeg" },
+      { path: "/travel/malaysia/15.jpeg" },
+    ],
+  },
+  {
+    category: "philippines",
+    categoryDisplayName: "🇵🇭 Philippines",
+    photos: [
+      { path: "/travel/philippines/4.jpeg" },
+      { path: "/travel/philippines/6.jpeg" },
+      { path: "/travel/philippines/7.jpeg" },
+      { path: "/travel/philippines/9.jpeg" },
+      { path: "/travel/philippines/3.jpeg" },
+      { path: "/travel/philippines/14.jpeg" },
+      { path: "/travel/philippines/15.jpeg" },
+    ],
+  },
+  {
+    category: "singapore",
+    categoryDisplayName: "🇸🇬 Singapore",
+    photos: [
+      { path: "/travel/singapore/1.jpeg" },
+      { path: "/travel/singapore/4.jpeg" },
+      { path: "/travel/singapore/8.jpeg" },
+      { path: "/travel/singapore/11.jpeg" },
+    ],
+  },
+  {
+    category: "taiwan",
+    categoryDisplayName: "🇹🇼 Taiwan",
+    photos: [
+      { path: "/travel/taiwan/1.jpeg" },
+      { path: "/travel/taiwan/2.jpeg" },
+      { path: "/travel/taiwan/5.jpeg" },
+      { path: "/travel/taiwan/6.jpeg" },
+      { path: "/travel/taiwan/12.jpeg" },
+      { path: "/travel/taiwan/17.jpeg" },
+    ],
+  },
+  {
+    category: "thailand",
+    categoryDisplayName: "🇹🇭 Thailand",
+    photos: [
+      { path: "/travel/thailand/2.jpeg" },
+      { path: "/travel/thailand/4.jpeg" },
+      { path: "/travel/thailand/6.jpeg" },
+      { path: "/travel/thailand/7.jpeg" },
+      { path: "/travel/thailand/8.jpeg" },
+      { path: "/travel/thailand/13.jpeg" },
+    ],
+  },
+  {
+    category: "usa",
+    categoryDisplayName: "🇺🇸 USA",
+    photos: [
+      { path: "/travel/usa/1.jpeg" },
+      { path: "/travel/usa/6.jpeg" },
+      { path: "/travel/usa/7.jpeg" },
+      { path: "/travel/usa/8.jpeg" },
+      { path: "/travel/usa/9.jpeg" },
+    ],
+  },
+  {
+    category: "vietnam",
+    categoryDisplayName: "🇻🇳 Vietnam",
+    photos: [
+      { path: "/travel/vietnam/11.jpeg" },
+      { path: "/travel/vietnam/10.jpeg" },
+      { path: "/travel/vietnam/8.jpeg" },
+      { path: "/travel/vietnam/7.jpeg" },
+      { path: "/travel/vietnam/4.jpeg" },
+      { path: "/travel/vietnam/1.jpeg" },
+    ],
+  },
+];
 
 export const galleries: Gallery[] = [
   {
     category: "latest",
     categoryDisplayName: "⚡ Latest photos",
-    photos: getPhotos("travel/latest"),
+    photos: getPhotosInFolder("travel/latest"),
   },
   {
     category: "cambodia",
     categoryDisplayName: "🇰🇭 Cambodia",
-    photos: getPhotos("travel/cambodia"),
+    photos: getPhotosInFolder("travel/cambodia"),
   },
   {
     category: "japan",
     categoryDisplayName: "🇯🇵 Japan",
-    photos: getPhotos("travel/japan"),
+    photos: getPhotosInFolder("travel/japan"),
   },
   {
     category: "malaysia",
     categoryDisplayName: "🇲🇾 Malaysia",
-    photos: getPhotos("travel/malaysia"),
+    photos: getPhotosInFolder("travel/malaysia"),
   },
   {
     category: "philippines",
     categoryDisplayName: "🇵🇭 Philippines",
-    photos: getPhotos("travel/philippines"),
+    photos: getPhotosInFolder("travel/philippines"),
   },
   {
     category: "singapore",
     categoryDisplayName: "🇸🇬 Singapore",
-    photos: getPhotos("travel/singapore"),
+    photos: getPhotosInFolder("travel/singapore"),
   },
   {
     category: "taiwan",
     categoryDisplayName: "🇹🇼 Taiwan",
-    photos: getPhotos("travel/taiwan"),
+    photos: getPhotosInFolder("travel/taiwan"),
   },
   {
     category: "thailand",
     categoryDisplayName: "🇹🇭 Thailand",
-    photos: getPhotos("travel/thailand"),
+    photos: getPhotosInFolder("travel/thailand"),
   },
   {
     category: "usa",
     categoryDisplayName: "🇺🇸 USA",
-    photos: getPhotos("travel/usa"),
+    photos: getPhotosInFolder("travel/usa"),
   },
   {
     category: "vietnam",
     categoryDisplayName: "🇻🇳 Vietnam",
-    photos: getPhotos("travel/vietnam"),
+    photos: getPhotosInFolder("travel/vietnam"),
   },
 ];
