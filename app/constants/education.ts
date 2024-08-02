3903607e352eddbdcd1c87ce01ecf2410f26f616
Yusuke <yusuke.suzuki.2024@gmail.com>
2024-08-02 16:42:32 +0900

    fixed: educations

--- app/constants/education.ts
+++ app/constants/education.ts
@@ -2,10 +2,6 @@ import { EducationType } from "../type/education";
 
 export const educations: EducationType[] = [
   {
-    period: "2013/04〜2016/03",
-    name: "静岡県立掛川工業高等学校",
-  },
-  {
     period: "2016/04〜2020/03",
     name: "静岡大学 情報学部 情報科学科",
   },
