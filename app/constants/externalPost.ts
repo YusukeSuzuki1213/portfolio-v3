ec74991403ee6966dff70122a1358ffea8d0529f
Yusuke <yusuke.suzuki.2024@gmail.com>
2025-01-30 09:19:00 +0900

    added: articles

--- app/constants/externalPost.ts
+++ app/constants/externalPost.ts
@@ -22,4 +22,15 @@ export const posts: ExternalPostType[] = [
       "https://cdn-ak.f.st-hatena.com/images/fotolife/v/vasilyjp/20210906/20210906154524.jpg",
     titleIconUrl: "/logo/android.svg",
   },
+  {
+    title:
+      "正社員→フリーランス→2度目の正社員。両方を経験したYusukeさんのキャリア選択のリアル",
+    description:
+      "今回は、Androidエンジニアとして、正社員とフリーランスを経験されたYusukeさんのインタビューです。正社員・フリーランスとして働いてきた経験や意思決定の背景などをリアルに語っていただきます。",
+    publishDate: "2025/01/24",
+    url: "https://techtrain.dev/media/articles/nj3f7bzinr9x",
+    imageUrl:
+      "https://images.microcms-assets.io/assets/bd8c4afb4ee8466a9989c560ceea8669/44d541fbf0874e67b55a19143ee78eb1/Yusuke%E3%81%95%E3%82%93_%E4%BF%AE%E6%AD%A3%E7%89%88.png",
+    titleIconUrl: "/logo/mic.png",
+  },
 ];
